33bcb4569ff5b16ffdd2051cd03b1aa02665a1e0
M. A. Muhaimin Sakib <sakib@appscode.com>
2020-04-02 16:40:22 +0600

    Initialize vue sfc
    Signed-off-by: M. A. Muhaimin Sakib <sakib@appscode.com>

---
+++ src/serve-dev.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VueSsr from '@/vue-ssr.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueSsr
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-ssr />
+  </div>
+</template>
